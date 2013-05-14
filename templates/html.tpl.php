c8661847447adbb43d17c43130d95f02f40ac297
Antonio Cucho <antonio.cucho@glr.pe>
2013-05-14 11:45:45 -0500

    Modified Styles an Html

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -2,28 +2,29 @@
 <!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7"<?php print $rdf_namespaces; ?>><![endif]-->
 <!--[if IE 7]><html class="no-js lt-ie9 lt-ie8"<?php print $rdf_namespaces; ?>><![endif]-->
 <!--[if IE 8]><html class="no-js lt-ie9"<?php print $rdf_namespaces; ?>><![endif]-->
-<!--[if gt IE 8]><!--><html class="no-js" <?php print $rdf_namespaces; ?>><!--<![endif]-->
-    <head profile="<?php print $grddl_profile; ?>">
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-        <meta name="viewport" content="width=device-width, initial-scale = 1.0, user-scalable = no">
-        <?php print $head; ?>
-        <title><?php print $head_title; ?></title>
-        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,600' rel='stylesheet' type='text/css'>
-        <?php print $styles; ?>        
-        <!-- <link rel="stylesheet" href="css/style.min.css" type="text/css" media="screen"> -->
-        <!--[if IE]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-    </head>
+<!--[if gt IE 8]><!-->
+<html class="no-js" <?php print $rdf_namespaces; ?>><!--<![endif]-->
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale = 1.0, user-scalable = no">
+    <?php print $head; ?>
+    <title><?php print $head_title; ?></title>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,600' rel='stylesheet' type='text/css'>
+    <?php print $styles; ?>        
+    <!-- <link rel="stylesheet" href="css/style.min.css" type="text/css" media="screen"> -->
+    <!--[if IE]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+  </head>
 
-    <body class="<?php print $classes; ?>" <?php print $attributes; ?>>
+  <body class="<?php print $classes; ?>" <?php print $attributes; ?>>
 
-        <?php print $page; ?>       
+    <?php print $page; ?>       
 
-        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-        <script type="text/javascript" src="http://www.google.com/jsapi"></script>
-        <script type="text/javascript">
-            google.load('visualization', '1', {packages: ['corechart']});
-        </script>
-        <?php print $scripts; ?>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 
-    </body>
+    <?php print $scripts; ?>
+    <script type="text/javascript" src="http://www.google.com/jsapi"></script>
+    <script type="text/javascript">
+      google.load('visualization', '1', {packages: ['corechart']});
+    </script>
+  </body>
 </html>
--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -1,85 +1,66 @@
 <div class="menu">
-    <div class="container clearfix">
+  <div class="container clearfix">
 
-        <?php if ($logo): ?>
-            <div id="logo" class="grid_3">
-                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-                    <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-                </a>
-            </div>
-        <?php endif; ?>
+    <?php if ($logo): ?>
+      <div id="logo" class="grid_2">
+        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+        </a>
+      </div>
+    <?php endif; ?>
 
-        <div id="nav" class="grid_9 omega">
+    <div id="nav" class="grid_10 omega">
+      <?php if ($page['header']) : ?>
+        <?php print drupal_render($page['header']); ?>
+      <?php endif; ?>
 
-            <?php if ($page['header']) : ?>
-                <?php print drupal_render($page['header']); ?>
-            <?php endif; ?>
-
-            <ul class="navigation">
-                <li data-slide="1">Home</li>
-                <li data-slide="2">About Lima I/O</li>
-                <li data-slide="3">Demo</li>
-                <li data-slide="4">Team</li>
-                <li data-slide="5">Contact</li>
-            </ul>
-        </div>
-
+      <ul class="navigation">
+        <li data-slide="1">Home</li>
+        <li data-slide="2">About us</li>
+        <li data-slide="3">Demo</li>
+        <li data-slide="4">Contact</li>                
+      </ul>
     </div>
+  </div>
 </div>
 
 
 <div class="slide" id="slide1" data-slide="1" data-stellar-background-ratio="0.5">
-    <div class="container clearfix">
-
-        <div id="content" class="grid_7">
-            <h1>Home</h1>
-        </div>
+  <div class="container clearfix">
+    <div id="content" class="grid_12">
+      <h1>Primer experimento peruano de Internet de las Cosas #IoT para monitoreo del medio ambiente.</h1>      
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
     </div>
+  </div>
+</div>
 
-
-
-    <div class="slide" id="slide2" data-slide="2" data-stellar-background-ratio="0.5">
-        <div class="container clearfix">
-
-            <div id="content" class="grid_12">
-                <h1>About Lima I/O</h1>            
-            </div>
-
-        </div>
+<div class="slide" id="slide2" data-slide="2" data-stellar-background-ratio="0.5">
+  <div class="container clearfix">
+    <div id="content" class="grid_12">
+      <h1>About us</h1>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
     </div>
+  </div>
+</div>
 
-
-
-    <div class="slide" id="slide3" data-slide="3" data-stellar-background-ratio="0.5">
-        <div class="container clearfix">
-
-            <div id="content" class="grid_12">
-                <h1>Demo</h1> 
-            </div>
-
-
-        </div>
+<div class="slide" id="slide3" data-slide="3" data-stellar-background-ratio="0.5">
+  <div class="container clearfix">
+    <div id="content" class="grid_12">
+      <h1>Demo</h1>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
     </div>
+  </div>
+</div>
 
 
 
-    <div class="slide" id="slide4" data-slide="4" data-stellar-background-ratio="0.5">
-        <div class="container clearfix">
-
-            <div id="content" class="grid_12">
-                <h1>Team</h1>
-            </div>
-
-        </div>
+<div class="slide" id="slide4" data-slide="4" data-stellar-background-ratio="0.5">
+  <div class="container clearfix">
+    <div id="content" class="grid_12">
+      <h1>Contact</h1>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
     </div>
-
-
-    <div class="slide" id="slide5" data-slide="4" data-stellar-background-ratio="0.5">
-        <div class="container clearfix">
-
-            <div id="content" class="grid_12">
-                <h1>Contact</h1>
-            </div>
-
-        </div>
-    </div>
+  </div>
+</div>
